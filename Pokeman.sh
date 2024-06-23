36940abd2ab3713a092dcdcf883e5589991ebfd3
Pavan <vspavan52@gmail.com>
2024-06-24 03:34:05 +0530

    newone

--- Pokeman.sh
+++ Pokeman.sh
@@ -1,4 +1,5 @@
-for Pokeman in $(curl -sL https://pokeapi.co/api/v2/pokemon?limit=1400 | jq ".results[].name" -r)
-do
-echo "The name of Pokeman Is: $Pokeman"
-done
+#!/bin/bash
+ALL_POKEMON=$(curl -sL https://pokeapi.co/api/v2/pokemon?limit=1400 | jq ".results[].name" -r)
+for pokeman in $ALL_POKEMON; do
+echo "Then Name of Pokeman IS: $pokeman"
+done 
